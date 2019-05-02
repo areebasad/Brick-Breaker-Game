3f959ca8b9279a5bd3585ca51f25c4c633a0be2f
Hafiz Areeb Asad <areebasad95@hotmail.com>
2019-05-02 17:08:41 +0200

    Pixii  Integration

---
+++ game.js
@@ -0,0 +1,17 @@
+let type = "WebGL"
+if(!PIXI.utils.isWebGLSupported()){
+  type = "canvas"
+}
+
+PIXI.utils.sayHello(type)
+
+//Create a Pixi Application
+let app = new PIXI.Application();
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
+
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoDensity = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
